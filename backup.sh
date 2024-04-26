6f5fe344ee7a9a6f07e96f8be2519fa187d11acd
Robert <rob06121@gmail.com>
2024-04-27 02:12:23 +0500

    fix

--- backup.sh
+++ backup.sh
@@ -6,12 +6,12 @@ BACKUP_FILE="bitwardenrs_$(date "+%F-%H%M%S")"
 # use sqlite3 to create backup (avoids corruption if db write in progress)
 cd ./data
 
-sqlite3 db.sqlite3 ".backup 'db-backup.sqlite3'"
+sqlite3 db.sqlite3 ".backup './db-backup.sqlite3'"
 
 sleep 20
 
 # tar up backup and encrypt with openssl and encryption key
-sudo -u root \
+
 tar -czf ${BACKUP_FILE}.tar.gz ./db-backup.sqlite3 attachments sends rsa_key*
 
 
