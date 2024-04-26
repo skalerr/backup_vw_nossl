767f35525729a31068bf3e2e3ff426ac53545ec6
Robert <rob06121@gmail.com>
2024-04-27 01:33:24 +0500

    add new path

--- backup.sh
+++ backup.sh
@@ -4,9 +4,8 @@
 BACKUP_FILE="bitwardenrs_$(date "+%F-%H%M%S")"
 
 # use sqlite3 to create backup (avoids corruption if db write in progress)
-sqlite3 /data/db.sqlite3 ".backup '/tmp/db.sqlite3'"
 
-sqlite3 /data/db.sqlite3 ".backup '/tmp/db-$(date '+%Y%m%d-%H%M').sqlite3'"
+sqlite3 /data/db.sqlite3 ".backup '/data/${BACKUP_FILE}.sqlite3'"
 
 # tar up backup and encrypt with openssl and encryption key
 tar -czf /tmp/${BACKUP_FILE}.tar.gz /data/attachments /data/sends /data/rsa_key*
@@ -15,7 +14,7 @@ tar -czf /tmp/${BACKUP_FILE}.tar.gz /data/attachments /data/sends /data/rsa_key*
 /dropbox_uploader.sh -f /config/.dropbox_uploader upload /tmp/${BACKUP_FILE}.tar.gz /${BACKUP_FILE}.tar.gz
 
 # cleanup tmp folder
-rm -rf /tmp/*
+rm /data/${BACKUP_FILE}.sqlite3*
 
 # delete older backups if variable is set & greater than 0
 if [ ! -z $DELETE_AFTER ] && [ $DELETE_AFTER -gt 0 ]
