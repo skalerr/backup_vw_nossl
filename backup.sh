c8e3d63bc54a0c15c314d73f84f2e6fbb6422be0
Robert <rob06121@gmail.com>
2024-04-27 01:31:38 +0500

    fix

--- backup.sh
+++ backup.sh
@@ -6,8 +6,10 @@ BACKUP_FILE="bitwardenrs_$(date "+%F-%H%M%S")"
 # use sqlite3 to create backup (avoids corruption if db write in progress)
 sqlite3 /data/db.sqlite3 ".backup '/tmp/db.sqlite3'"
 
+sqlite3 /data/db.sqlite3 ".backup '/tmp/db-$(date '+%Y%m%d-%H%M').sqlite3'"
+
 # tar up backup and encrypt with openssl and encryption key
-tar -czf /tmp/${BACKUP_FILE}.tar.gz /data/attachments /data/sends /data/config.json /data/rsa_key*
+tar -czf /tmp/${BACKUP_FILE}.tar.gz /data/attachments /data/sends /data/rsa_key*
 
 # upload encrypted tar to dropbox
 /dropbox_uploader.sh -f /config/.dropbox_uploader upload /tmp/${BACKUP_FILE}.tar.gz /${BACKUP_FILE}.tar.gz
